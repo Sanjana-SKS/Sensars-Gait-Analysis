6e204493e54e40bbbbd6747b5a1e81d5c09ed247
soumikaseelam <soumikaseelam.edu@gmail.com>
2025-02-04 19:49:14 -0600

    adding all firebase sdk integration

---
+++ src/firebase/firebaseConfig.ts
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyAQ-_I_sF2RoK7wWH9-m25bk6wtwLeP2Y4",
+  authDomain: "sensars-gaitanalysistol.firebaseapp.com",
+  projectId: "sensars-gaitanalysistol",
+  storageBucket: "sensars-gaitanalysistol.firebasestorage.app",
+  messagingSenderId: "267727656305",
+  appId: "1:267727656305:web:6e568bcf6b34bb6ce792d0",
+  measurementId: "G-F3M2E2JSD2"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
